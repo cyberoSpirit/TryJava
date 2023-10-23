a9da6827c57ee86b17d76b006ddd4b20db319064
Julia <julia.hrytsyk@gmail.com>
2023-10-23 21:17:22 +0200

    Task 1. Print initials.

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my dear!");
+        System.out.println("How are you today?");
+        PrintInitials();
+
+        //javac <FileName.java>
+        //java <FileName>
     }
-    
+
+    //Prints initials of cyberoSpirit
+    public static void PrintInitials() {
+        System.out.println(" CCCC     SSSS ");
+        System.out.println("C    C   S    S");
+        System.out.println("C         S");
+        System.out.println("C          S");
+        System.out.println("C           S");
+        System.out.println("C    C   S    S");
+        System.out.println(" CCCC     SSSS");
+    }
 }
