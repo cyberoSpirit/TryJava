c01d5c58500b4b222eba045343bfe1bf9bae437f
Julia <julia.hrytsyk@gmail.com>
2023-10-24 21:59:30 +0200

    Survey finisched. Counter added.

--- Main.java
+++ Main.java
@@ -11,28 +11,34 @@ public class Main {
     }
 
     public static void Survey() {
+        int counter = 0;
         System.out.println("Welcome. Thank you for taking the survey");
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What is your name?");
         String name = scanner.nextLine();
+        counter++;
 
         System.out.println("How much money do you spend on coffee?");
         double coffeePrice = scanner.nextDouble();
+        ++counter;
 
         System.out.println("How much money do you spend on fast food?");
         double foodPrice = scanner.nextDouble();
+        counter++;
 
 
         System.out.println("How many times a week do you buy coffee?");
         int coffeeAmount = scanner.nextInt();
+        counter++;
 
         System.out.println("How many times a week do you buy fast food?");
         int foodAmount = scanner.nextInt();
+        counter++;
         scanner.close();
 
-        //*********PART B: RESPONDING TO THE USER**********
-        System.out.println("Thank you " + name + " for answering all <counter> questions");
+        //RESPOND TO THE USER
+        System.out.println("Thank you " + name + " for answering all " + counter + " questions");
         System.out.println("Weekly, you spend $" + (coffeePrice * coffeeAmount) + " on coffee");
         System.out.println("Weekly, you spend $" + (foodPrice * foodAmount) + " on food");
     }
