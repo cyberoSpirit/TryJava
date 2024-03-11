bcb9e0338b709707080108f151ec84c37e80876d
Julia <julia.hrytsyk@gmail.com>
2024-03-11 12:52:18 +0100

    Javagram method developed to test NextLineTrap

--- Main.java
+++ Main.java
@@ -2,13 +2,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        NextLineExperiment();
+        Javagram();
+        //StrangeNamedVariables();
+        //NextLineExperiment();
         //Survey();
         //GryffindorPoints();
         //PrintInitials();
 
         //javac <FileName.java>
         //java <FileName>
+    }
+
+    public static void Javagram() {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Welcome to JavaGram! Let's sign you up.");
+        System.out.println("What is your first name?");
+        String firstName = scan.nextLine();
+
+        System.out.println("What is your last name?");
+        String lastName = scan.nextLine();
+
+        System.out.println("How old are you?");
+        int age = scan.nextInt();
+
+        System.out.println("Make a username");  
+        // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
+        scan.nextLine();
+        String username = scan.nextLine();
+        
+        System.out.println("What city do you live in?");
+        String city = scan.nextLine();
+
+        System.out.println("What country is that?");
+        String country = scan.nextLine();
+        
+        System.out.println("Thank you for joining JavaGram!");
+        System.out.println("\nHere is the information you entered:");
+        System.out.println("\tFirst Name: " + firstName);
+        System.out.println("\tLast Name: " + lastName);
+        System.out.println("\tAge: " + age);
+        System.out.println("\tUsername: " + username);
+        System.out.println("\tCity: " + city);
+        System.out.println("\tCountry: " + country);
+
+        scan.close();
+    }
+    
+    public static void StrangeNamedVariables()
+    {
+        //Because Java is good localized
+        double π = 3.14159;
+        System.out.println(π);
+        String 你好 = "你好世界";
+        System.out.println(你好);
+        int лв = 1;
+        System.out.println(лв);
     }
 
     public static void NextLineExperiment() {
@@ -29,7 +78,7 @@ public class Main {
         System.out.println("Weekly, you spend $" + (foodPrice * foodAmount) + " on food");
     }
 
-    public static void Survey() {
+    public static void Survey() { 
         int counter = 0;
         System.out.println("Welcome. Thank you for taking the survey");
         Scanner scanner = new Scanner(System.in);
