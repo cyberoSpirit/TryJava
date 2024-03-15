e7e6e2bf64450d957f75ce76e3de5d576b28d33d
Julia <julia.hrytsyk@gmail.com>
2024-03-15 17:12:10 +0100

    Boolean moved to a separate file.

---
+++ Basics/Basic.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+
+public class Basic {
+    public static void main(String[] args) {
+        //Basics. Types and Variables.
+        //strangeNamedVariables();
+        //nextLineExperiment();
+        //javagram();
+        //survey();
+        //gryffindorPoints();
+        //printInitials();
+
+        //javac <FileName.java>
+        //java <FileName>
+
+        //Boolean and Conditions
+        //var toy = new BooleansAndConditions();
+        //toy.bank();
+        //toy.calendar();
+        //toy.booleanComparisons();
+        //toy.javaMart();
+        //toy.championship();
+        
+    }
+
+    //Basics
+    public static void strangeNamedVariables()
+    {
+        //Because Java is good localized
+        double π = 3.14159;
+        System.out.println(π);
+        String 你好 = "你好世界";
+        System.out.println(你好);
+        int лв = 1;
+        System.out.println(лв);
+    }
+
+    public static void nextLineExperiment() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How much money do you spend on food?");
+        double foodPrice = scanner.nextDouble();
+
+        System.out.println("How many times a week do you buy food?");
+        int foodAmount = scanner.nextInt();
+
+        System.out.println("What is your name?");
+        scanner.nextLine(); //To be wasted on empty space
+        String name = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("Thank you " + name + " for answering all questions");
+        System.out.println("Weekly, you spend $" + (foodPrice * foodAmount) + " on food");
+    }
+
+    //Kind of Survey
+    public static void javagram() {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Welcome to JavaGram! Let's sign you up.");
+    System.out.println("What is your first name?");
+    String firstName = scan.nextLine();
+
+    System.out.println("What is your last name?");
+    String lastName = scan.nextLine();
+
+    System.out.println("How old are you?");
+    int age = scan.nextInt();
+
+    System.out.println("Make a username");  
+    // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
+    scan.nextLine();
+    String username = scan.nextLine();
+    
+    System.out.println("What city do you live in?");
+    String city = scan.nextLine();
+
+    System.out.println("What country is that?");
+    String country = scan.nextLine();
+    
+    System.out.println("Thank you for joining JavaGram!");
+    System.out.println("\nHere is the information you entered:");
+    System.out.println("\tFirst Name: " + firstName);
+    System.out.println("\tLast Name: " + lastName);
+    System.out.println("\tAge: " + age);
+    System.out.println("\tUsername: " + username);
+    System.out.println("\tCity: " + city);
+    System.out.println("\tCountry: " + country);
+
+    scan.close();
+}
+
+    public static void survey() { 
+        int counter = 0;
+        System.out.println("Welcome. Thank you for taking the survey");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        counter++;
+
+        System.out.println("How much money do you spend on coffee?");
+        double coffeePrice = scanner.nextDouble();
+        ++counter;
+
+        System.out.println("How much money do you spend on fast food?");
+        double foodPrice = scanner.nextDouble();
+        counter++;
+
+
+        System.out.println("How many times a week do you buy coffee?");
+        int coffeeAmount = scanner.nextInt();
+        counter++;
+
+        System.out.println("How many times a week do you buy fast food?");
+        int foodAmount = scanner.nextInt();
+        counter++;
+        scanner.close();
+
+        //RESPOND TO THE USER
+        System.out.println("Thank you " + name + " for answering all " + counter + " questions");
+        System.out.println("Weekly, you spend $" + (coffeePrice * coffeeAmount) + " on coffee");
+        System.out.println("Weekly, you spend $" + (foodPrice * foodAmount) + " on food");
+    }
+
+    public static void gryffindorPoints() {
+        int maxValue = 2147483647;
+        System.out.println(maxValue);
+
+        int points = 0;
+
+        System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
+        points -= 50;
+        System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
+        points -= 3;
+        System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
+        points += 30;
+        System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
+        points += 100;
+        System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
+        points += 60;        
+        
+        System.out.println("Gryffindor received :" + points + " points");
+        
+    }
+
+    //Prints initials of cyberoSpirit
+    public static void printInitials() {
+        System.out.println(" CCC     SSS ");
+        System.out.println("C   C   S   S");
+        System.out.println("C        S   ");
+        System.out.println("C         S  ");
+        System.out.println("C          S ");
+        System.out.println("C   C   S   S");
+        System.out.println(" CCC     SSS ");
+    }
+}
--- Basics/BooleansAndConditions.java
+++ Basics/BooleansAndConditions.java
@@ -1,30 +1,7 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-
-
-        //Boolean and Conditions
-        //bank();
-        //calendar();
-        //booleanComparisons();
-        //javaMart();
-        //championship();
-
-        //Basics. Types and Variables.
-        //strangeNamedVariables();
-        //nextLineExperiment();
-        //javagram();
-        //survey();
-        //gryffindorPoints();
-        //printInitials();
-
-        //javac <FileName.java>
-        //java <FileName>
-    }
-
-    //Boolean and Conditions
-    public static void bank() {
+public class BooleansAndConditions {
+    public void bank() {
         Scanner scan = new Scanner(System.in);
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
@@ -55,7 +32,7 @@ public class Main {
         scan.close();
     }
 
-    public static void calendar() {
+    public void calendar() {
         System.out.println("Which day do you ask about?");
         Scanner sc = new Scanner(System.in);
         String day = sc.nextLine();
@@ -92,7 +69,7 @@ public class Main {
         sc.close();
     }
 
-    public static void booleanComparisons() {
+    public void booleanComparisons() {
         
         int chemistryGrade = 95;
         int biologyGrade = 75;
@@ -159,7 +136,7 @@ public class Main {
         System.out.println("Java: " + !thirdWord.equals(fourthWord) + "\n");
     }
 
-    public static void javaMart() {
+    public void javaMart() {
         double wallet = 100;
 
         double toyCar = 5.99;
@@ -182,7 +159,7 @@ public class Main {
         }
     }
 
-    public static void championship() {
+    public void championship() {
 
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
@@ -202,135 +179,4 @@ public class Main {
         }
     }
 
-    //Basics
-    public static void strangeNamedVariables()
-    {
-        //Because Java is good localized
-        double π = 3.14159;
-        System.out.println(π);
-        String 你好 = "你好世界";
-        System.out.println(你好);
-        int лв = 1;
-        System.out.println(лв);
-    }
-
-    public static void nextLineExperiment() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("How much money do you spend on food?");
-        double foodPrice = scanner.nextDouble();
-
-        System.out.println("How many times a week do you buy food?");
-        int foodAmount = scanner.nextInt();
-
-        System.out.println("What is your name?");
-        scanner.nextLine(); //To be wasted on empty space
-        String name = scanner.nextLine();
-        scanner.close();
-
-        System.out.println("Thank you " + name + " for answering all questions");
-        System.out.println("Weekly, you spend $" + (foodPrice * foodAmount) + " on food");
-    }
-
-    //Kind of Survey
-    public static void javagram() {
-    Scanner scan = new Scanner(System.in);
-    
-    System.out.println("Welcome to JavaGram! Let's sign you up.");
-    System.out.println("What is your first name?");
-    String firstName = scan.nextLine();
-
-    System.out.println("What is your last name?");
-    String lastName = scan.nextLine();
-
-    System.out.println("How old are you?");
-    int age = scan.nextInt();
-
-    System.out.println("Make a username");  
-    // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
-    scan.nextLine();
-    String username = scan.nextLine();
-    
-    System.out.println("What city do you live in?");
-    String city = scan.nextLine();
-
-    System.out.println("What country is that?");
-    String country = scan.nextLine();
-    
-    System.out.println("Thank you for joining JavaGram!");
-    System.out.println("\nHere is the information you entered:");
-    System.out.println("\tFirst Name: " + firstName);
-    System.out.println("\tLast Name: " + lastName);
-    System.out.println("\tAge: " + age);
-    System.out.println("\tUsername: " + username);
-    System.out.println("\tCity: " + city);
-    System.out.println("\tCountry: " + country);
-
-    scan.close();
-}
-
-    public static void survey() { 
-        int counter = 0;
-        System.out.println("Welcome. Thank you for taking the survey");
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        counter++;
-
-        System.out.println("How much money do you spend on coffee?");
-        double coffeePrice = scanner.nextDouble();
-        ++counter;
-
-        System.out.println("How much money do you spend on fast food?");
-        double foodPrice = scanner.nextDouble();
-        counter++;
-
-
-        System.out.println("How many times a week do you buy coffee?");
-        int coffeeAmount = scanner.nextInt();
-        counter++;
-
-        System.out.println("How many times a week do you buy fast food?");
-        int foodAmount = scanner.nextInt();
-        counter++;
-        scanner.close();
-
-        //RESPOND TO THE USER
-        System.out.println("Thank you " + name + " for answering all " + counter + " questions");
-        System.out.println("Weekly, you spend $" + (coffeePrice * coffeeAmount) + " on coffee");
-        System.out.println("Weekly, you spend $" + (foodPrice * foodAmount) + " on food");
-    }
-
-    public static void gryffindorPoints() {
-        int maxValue = 2147483647;
-        System.out.println(maxValue);
-
-        int points = 0;
-
-        System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
-        points -= 50;
-        System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
-        points -= 3;
-        System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
-        points += 30;
-        System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
-        points += 100;
-        System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
-        points += 60;        
-        
-        System.out.println("Gryffindor received :" + points + " points");
-        
-    }
-
-    //Prints initials of cyberoSpirit
-    public static void printInitials() {
-        System.out.println(" CCC     SSS ");
-        System.out.println("C   C   S   S");
-        System.out.println("C        S   ");
-        System.out.println("C         S  ");
-        System.out.println("C          S ");
-        System.out.println("C   C   S   S");
-        System.out.println(" CCC     SSS ");
-    }
 }
