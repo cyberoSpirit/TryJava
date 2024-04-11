9bad3a7b224f466dde483f57d6385b46086edc6c
cyberoSpirit <julia.hrytsyk@gmail.com>
2024-04-11 11:18:54 +0200

    Start Capstone project to test all skills from Module 2.

---
+++ Special/Capstone/TryCapstone.java
@@ -0,0 +1,5 @@
+public class TryCapstone {
+    public static void TestCapstone() {
+        
+    }
+}
--- Special/Main.java
+++ Special/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //TestLambda.TryLambda();
         //TestBigDecimal.TryBigDecimal();
-        TryInterfaces.TestInterfaces();
+        //TryInterfaces.TestInterfaces();
+        TryCapstone.TestCapstone();
     }
 }
