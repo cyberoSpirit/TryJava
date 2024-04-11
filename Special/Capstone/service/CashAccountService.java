182e94604d71ab0a59d5d2d3ec87a35400b3cbbf
cyberoSpirit <julia.hrytsyk@gmail.com>
2024-04-11 12:30:16 +0200

    Capstone project part 3 completed.

--- Special/Capstone/service/CashAccountService.java
+++ Special/Capstone/service/CashAccountService.java
@@ -1,7 +1,59 @@
 package service;
 
+import java.math.BigDecimal;
+import java.util.NoSuchElementException;
 
-public class CashAccountService {
+import pojo.CashAccount;
+import repository.TradeAccountRepository;
 
+public class CashAccountService implements TradeAccountService{
+    private TradeAccountRepository repository;
 
+    public CashAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        CashAccount account = (CashAccount) this.repository.retrieveTradeAccount(id);
+        if (account == null) {
+            throw new NoSuchElementException("Element with the id " + id + " was not found.");
+        } else {
+            account.setCashBalance(account.getCashBalance().add(amount));
+        }
+        updateTradeAccount(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        CashAccount account = (CashAccount) this.repository.retrieveTradeAccount(id);
+        if (account == null) {
+            throw new NoSuchElementException("Element with the id " + id + " was not found.");
+        } else {
+            account.setCashBalance(account.getCashBalance().subtract(amount));
+        }
+        updateTradeAccount(account);
+    }
+
+    public void createTradeAccount(CashAccount account) {
+        if (account != null) {
+            repository.createTradeAccount(account);
+        }
+    }
+
+    public CashAccount retrieveTradeAccount(String id) {
+        return (CashAccount) this.repository.retrieveTradeAccount(id);
+    }
+
+    public void updateTradeAccount(CashAccount account) {
+        if (account != null) {
+            this.repository.updateTradeAccount(account);
+        }
+    }
+
+    public CashAccount deleteTradeAccount(String id) {
+        CashAccount account = retrieveTradeAccount(id);
+        this.repository.deleteTradeAccount(id);
+        return account;
+    }
 }
--- Special/Capstone/service/MarginAccountService.java
+++ Special/Capstone/service/MarginAccountService.java
@@ -1,7 +1,59 @@
 package service;
 
+import java.math.BigDecimal;
+import java.util.NoSuchElementException;
 
-public class MarginAccountService {
+import pojo.MarginAccount;
+import repository.TradeAccountRepository;
 
+public class MarginAccountService implements TradeAccountService {
+    private TradeAccountRepository repository;
 
+    public MarginAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        MarginAccount account = (MarginAccount) this.repository.retrieveTradeAccount(id);
+        if (account == null) {
+            throw new NoSuchElementException("Element with the id " + id + " was not found.");
+        } else {
+            account.setMargin(account.getMargin().add(amount));
+        }
+        updateTradeAccount(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        MarginAccount account = (MarginAccount) this.repository.retrieveTradeAccount(id);
+        if (account == null) {
+            throw new NoSuchElementException("Element with the id " + id + " was not found.");
+        } else {
+            account.setMargin(account.getMargin().subtract(amount));
+        }
+        updateTradeAccount(account);
+    }
+
+    public void createTradeAccount(MarginAccount account) {
+        if (account != null) {
+            repository.createTradeAccount(account);
+        }
+    }
+
+    public MarginAccount retrieveTradeAccount(String id) {
+        return (MarginAccount) this.repository.retrieveTradeAccount(id);
+    }
+
+    public void updateTradeAccount(MarginAccount account) {
+        if (account != null) {
+            this.repository.updateTradeAccount(account);
+        }
+    }
+
+    public MarginAccount deleteTradeAccount(String id) {
+        MarginAccount account = retrieveTradeAccount(id);
+        this.repository.deleteTradeAccount(id);
+        return account;
+    }
 }
